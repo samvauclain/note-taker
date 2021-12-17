8b96fecb5d065035dd09b8834d8fe88c9bf8b766
Sam Place <samvauclain@gmail.com>
2021-12-16 23:28:32 -0800

    started id functionality

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-const db  = require('./db/db.json');
+const { notes }  = require('./db/db.json');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -13,11 +13,21 @@ app.use(express.static('public'));
 // API notes route
 app.get('/api/notes', (req, res) => {
     // res.send('Hello!');
-    let results = db.notes;
+    let results = notes;
     console.log(req.query);
 
     res.json(results);
 });
+
+app.get('/api/notes/:id', (req, res) => {
+    const result = findById(req.params.id, notes);
+      res.json(result);
+  });
+
+function findById(id, notesArray) {
+    const result = notesArray.filter(note => note.id === id)[0];
+    return result;
+  }
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
